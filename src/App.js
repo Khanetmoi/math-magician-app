e166a5f33f9ca3bff21b91fdd039dc9be1e4aa6d
Inayat-khan <khanetmoi@gmail.com>
2023-01-27 16:52:56 +0000

    full app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calculator from './component/Calculator';
+import Home from './component/Home';
+import Quote from './component/Quote';
+import Nav from './component/Nav';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Calculator />
-      </div>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Calculator" element={<Calculator />} />
+          <Route path="/Quote" element={<Quote />} />
+        </Routes>
+      </Router>
+
     );
   }
 }
